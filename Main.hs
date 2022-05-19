31890bda5e75ace533d87dbb0c717d243107f545
MiniaczQ <MiniaczQ@users.noreply.github.com>
2022-05-19 21:20:12 +0200

    remove stuff

--- Main.hs
+++
@@ -1,8 +0,0 @@
-import Core (gameLoop)
-import Console (printLines)
-import Data (splashText, menuText, initialGameState)
-
-main = do
-  printLines splashText
-  printLines menuText
-  gameLoop initialGameState
