9ade4071596f50de55ba4b311825d0f698471671
MiniaczQ <MiniaczQ@users.noreply.github.com>
2022-05-20 02:30:49 +0200

    item desc

--- src/commands/CommandAppr.hs
+++ src/commands/CommandAppr.hs
@@ -17,20 +17,24 @@ appraise item = do
 
 missingItem :: String -> StateT GameState IO ()
 missingItem item = do
-  r <- randInt (0, 4)
+  r <- randInt (0, 2)
   case r of
-    0 -> liftIO $ println $ "What is a `" ++ item ++ "`?"
-    1 -> liftIO $ println $ "You must remember `" ++ item ++ "` from your childhood."
-    2 -> liftIO $ println $ "PLACEHOLDER " ++ item
-    3 -> liftIO $ println $ "PLACEHOLDER " ++ item
-    4 -> liftIO $ println $ "PLACEHOLDER " ++ item
-    _ -> liftIO $ println $ "DEFAULT PLACEHOLDER " ++ item
+    0 -> liftIO $ println "There is nothing to appraise."
+    1 -> liftIO $ println $ "There is no `" ++ item ++ "` in your pocket."
+    _ -> liftIO $ println $ "You don't own `" ++ item ++ "`. Whetever it may be."
   return ()
 
 describeItem :: String -> StateT GameState IO ()
 describeItem item = do
   case item of
-    "balls" -> liftIO $ println "nuts even, testicles if you may"
-    "" -> liftIO $ println ""
-    _ -> liftIO $ println "how did you get here??"
+    "rum" -> liftIO $ println "Would be delicious with Cola, if I knew what that was."
+    "gunpowder" -> liftIO $ println "Let's hope it doesn't explode."
+    "blunderbuss" -> liftIO $ println "I like to call it the Blunderbussy."
+    "ration" -> liftIO $ println "Meat so dry you can rip pants with it."
+    "banana" -> liftIO $ println "A source of comedy. Especially when rotated."
+    "map_piece_1" -> liftIO $ println "A map piece that appears to be torn in half. Maybe I can find the other piece if I ask merchants?"
+    "map_piece_2" -> liftIO $ println "A map piece that appears to be torn in half. Maybe the pirates know something about the other piece?"
+    "mercenary" -> liftIO $ println "The power of Sun Tzu in the palm of your hand. Reduces pirate attacks and deflects them."
+    "coin" -> liftIO $ println "Local currency. I think it's named 'Kromer'?"
+    _ -> liftIO $ println "-unreachable-entry-"
   return ()
